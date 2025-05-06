f21ba9e26e7901fb58bec13b1cde695b438e6a78
Wardrod-Mine <wardrodmine@gmail.com>
2025-05-06 13:22:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './ProductList';
+import DataForm from './DataForm';
+window.Telegram.WebApp 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/form" element={<DataForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
 
 function App() {
-    const tg = window.Telegram.WebApp;
+    const tg = window.Telegram ? window.Telegram.WebApp : null;
+
 
     useEffect(() => {
         tg.ready();
